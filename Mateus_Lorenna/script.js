7a3a613fb3ee68cd489a69cb43d0c348d68e4ed7
unknown <mateus@tminformatica.com.br>
2023-02-27 15:24:23 -0300

    inital commit

---
+++ Mateus_Lorenna/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+        document.title = "Volta aqui :(";
+    } else {
+        document.title = "MATEUS E LORENNA";
+    }
+});
